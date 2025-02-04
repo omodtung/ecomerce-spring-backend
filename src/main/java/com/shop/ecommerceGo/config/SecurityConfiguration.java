d86cad934dd1c70cc03a0dbcbab962afe634b88d
TungDT <dothedung131@gmail.com>
2025-02-04 22:21:52 +0700

    login custom with page own in auth

--- src/main/java/com/shop/ecommerceGo/config/SecurityConfiguration.java
+++ src/main/java/com/shop/ecommerceGo/config/SecurityConfiguration.java
@@ -2,15 +2,18 @@ package com.shop.ecommerceGo.config;
 
 import com.shop.ecommerceGo.service.CustomUserDetailsService;
 import com.shop.ecommerceGo.service.UserService;
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -26,18 +29,61 @@ public class SecurityConfiguration {
     return new CustomUserDetailsService(userService);
   }
 
+  // Dieu huong ben trong trang login cua spring
+  //   @Bean
+  //   public AuthenticationManager authenticationManager(
+  //     HttpSecurity http,
+  //     PasswordEncoder passwordEncoder,
+  //     UserDetailsService userDetailsService
+  //   ) throws Exception {
+  //     AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
+  //       AuthenticationManagerBuilder.class
+  //     );
+  //     authenticationManagerBuilder
+  //       .userDetailsService(userDetailsService)
+  //       .passwordEncoder(passwordEncoder);
+  //     return authenticationManagerBuilder.build();
+  //   }
+
   @Bean
-  public AuthenticationManager authenticationManager(
-    HttpSecurity http,
+  public DaoAuthenticationProvider authProvider(
     PasswordEncoder passwordEncoder,
     UserDetailsService userDetailsService
-  ) throws Exception {
-    AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
-      AuthenticationManagerBuilder.class
-    );
-    authenticationManagerBuilder
-      .userDetailsService(userDetailsService)
-      .passwordEncoder(passwordEncoder);
-    return authenticationManagerBuilder.build();
+  ) {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder);
+    // authProvider.setHideUserNotFoundExceptions(false);
+    return authProvider;
+  }
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // v6. lamda
+    http
+      .authorizeHttpRequests(authorize ->
+        authorize
+          .dispatcherTypeMatchers(
+            DispatcherType.FORWARD,
+            DispatcherType.INCLUDE
+          )
+          .permitAll()
+          .requestMatchers(
+            "/",
+            "/login",
+            "/client/**",
+            "/css/**",
+            "/js/**",
+            "/images/**"
+          )
+          .permitAll()
+          .anyRequest()
+          .authenticated()
+      )
+      .formLogin(formLogin ->
+        formLogin.loginPage("/login").failureUrl("/login?error").permitAll()
+      );
+
+    return http.build();
   }
 }
