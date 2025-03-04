d8dbf931d2afdd215b6f6be05bab78995b122e0a
TungDT <dothedung131@gmail.com>
2025-03-04 21:17:36 +0700

    product repository findAll with two input Method

--- src/main/java/com/shop/ecommerceGo/repository/ProductRepository.java
+++ src/main/java/com/shop/ecommerceGo/repository/ProductRepository.java
@@ -5,9 +5,11 @@ import com.shop.ecommerceGo.domain.User;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
   Product save(Product pr);
@@ -15,4 +17,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
   List<Product> findAll();
   Optional<Product> findById(long id);
+
+    Page<Product> findAll(Pageable page);
+
+    Page<Product> findAll(Specification<Product> spec, Pageable page);
 }
