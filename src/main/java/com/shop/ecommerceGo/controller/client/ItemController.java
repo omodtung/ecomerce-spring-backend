0acc0d4707466937fd3fe56e00ac97815e463650
TungDT <dothedung131@gmail.com>
2025-03-04 21:16:46 +0700

    productPage to sort

--- src/main/java/com/shop/ecommerceGo/controller/client/ItemController.java
+++ src/main/java/com/shop/ecommerceGo/controller/client/ItemController.java
@@ -3,13 +3,21 @@ package com.shop.ecommerceGo.controller.client;
 import com.shop.ecommerceGo.domain.Cart;
 import com.shop.ecommerceGo.domain.CartDetail;
 import com.shop.ecommerceGo.domain.Product;
+import com.shop.ecommerceGo.domain.Product_;
 import com.shop.ecommerceGo.domain.User;
+import com.shop.ecommerceGo.domain.dto.*;
+import com.shop.ecommerceGo.domain.dto.ProductCriteriaDTO;
 import com.shop.ecommerceGo.service.ProductService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.tags.shaded.org.apache.regexp.recompile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ItemController {
 
+  ProductCriteriaDTO productCriteriaDTO = new ProductCriteriaDTO();
   private final ProductService productService;
 
   public ItemController(ProductService productService) {
@@ -45,7 +54,7 @@ public class ItemController {
     long productId = id;
 
     String email = (String) session.getAttribute("email");
-    this.productService.handleAddProductToCart(email, productId, session);
+    this.productService.handleAddProductToCart(email, productId, session, 1);
 
     return "redirect:/";
   }
@@ -144,5 +153,53 @@ public class ItemController {
   @GetMapping("/thanks")
   public String getThankYouPage(Model model) {
     return "client/cart/thanks";
+  }
+
+  @GetMapping("/products")
+  public String getProductPage(
+    Model model,
+    ProductCriteriaDTO productCriteriaDTO,
+    HttpServletRequest request
+  ) {
+    int page = 1;
+    try {
+      if (productCriteriaDTO.getPage().isPresent()) {
+        // convert from String to int
+        page = Integer.parseInt(productCriteriaDTO.getPage().get());
+      } else {
+        // page = 1
+      }
+    } catch (Exception e) {}
+    Pageable pageable = PageRequest.of(page - 1, 10);
+    if (
+      productCriteriaDTO.getSort() != null &&
+      productCriteriaDTO.getSort().isPresent()
+    ) {
+      String sort = productCriteriaDTO.getSort().get();
+      if (sort.equals("gia-tang-dan")) {
+        pageable =
+          PageRequest.of(page - 1, 10, Sort.by(Product_.PRICE).ascending());
+      } else if (sort.equals("gia-giam-dan")) {
+        pageable =
+          PageRequest.of(page - 1, 10, Sort.by(Product_.PRICE).descending());
+      }
+    }
+    Page<Product> prs =
+      this.productService.fetchProductsByPage(pageable, productCriteriaDTO);
+    List<Product> products = prs.getContent().size() > 0
+      ? prs.getContent()
+      : new ArrayList<Product>();
+
+    String qs = request.getQueryString();
+    if (qs != null && !qs.isBlank()) {
+      // remove page
+      qs = qs.replace("page=" + page, "");
+    }
+    model.addAttribute("products", products);
+    model.addAttribute("currentPage", page);
+    model.addAttribute("totalPages", prs.getTotalPages());
+    model.addAttribute("queryString", qs);
+
+    return "client/product/show";
   }
 }
